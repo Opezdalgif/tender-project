238d18b423ae6cd18a3628ecf55ddca79455ae28
Opezdalgif <71887068+Opezdalgif@users.noreply.github.com>
2022-11-14 14:05:36 +0500

    Включение CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const PORT = process.env.WEBSERVER_PORT || 5000;
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
